9b721699edce1be33f098b1f67bf37a2a1a6063e
im85288 <ianmalcolmmclaughlin@gmail.com>
2016-12-20 19:59:48 +0000

    starting

--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'emby.kuroskin',
+        name: 'Kuro Emby Skin'
     };
 });
